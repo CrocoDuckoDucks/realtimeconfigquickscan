22afd536de74af931b9cec947ff147e15b03ec52
CrocoDuck <CrocoDuckoDucks@users.noreply.github.com>
2016-03-27 16:27:32 +0100

    Adding Distribution Identifier Subroutine

--- Identify_Distro.pm
+++ Identify_Distro.pm
@@ -1,3 +1,20 @@
+#   Copyright 2010 Arnout Engelen
+#
+#     This file is part of realtimeconfigquickscan.
+#
+#    realtimeconfigquickscan is free software: you can redistribute it and/or
+#    modify it under the terms of the GNU General Public License as published
+#    by the Free Software Foundation, either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    realtimeconfigquickscan is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with realtimeconfigquickscan.
+#    If not, see <http://www.gnu.org/licenses/>.
 sub Identify_Distro {
 
 # Try to detect which distribution we are running on. Search for /etc/os-release
